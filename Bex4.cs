240aaa9fa7c24e63b8d9f861989660ea1d43d7ff
Moonyll <s.pouwel@laposte.net>
2018-12-18 13:00:49 +0100

    Bex4

---
+++ Bex4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nombre;
+            for (nombre = 10; nombre >= 0; nombre--)
+            {
+                Console.WriteLine(nombre);
+            }
+        }
+    }
+}
